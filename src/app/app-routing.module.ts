5f74bbbea97e97158bb43bb64b471959bf4767ec
SALLT-VLADIMIRS\vladimir.salguero <vladimir.salguero@clearviewlive.com>
2024-01-22 23:40:53 -0600

    feat: adding routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListadoMascotaComponent } from './components/listado-mascota/listado-mascota.component';
+import { AgregarEditarMascotaComponent } from './components/agregar-editar-mascota/agregar-editar-mascota.component';
+import { VerMascotaComponent } from './components/ver-mascota/ver-mascota.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'listMascotas', pathMatch: 'full' },
+  { path: 'listMascotas', component: ListadoMascotaComponent },
+  { path: 'agregarMascota', component: AgregarEditarMascotaComponent },
+  { path: 'verMascota/:id', component: VerMascotaComponent },
+  { path: 'editarMascota/:id', component: AgregarEditarMascotaComponent },
+  { path: '**', redirectTo: 'listMascotas', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
